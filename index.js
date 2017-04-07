2f9dfc8dde654008d4cc8caa0d9d6e90f175041a
Seb Gertz <sebgertz@gmail.com>
2017-04-07 17:41:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 function getFirstSelector(selector) {
-if (selector == querySelector) {
-  return selector;
+    var element = document.querySelector(selector);
+    return element;
 }
+
+function nestedTarget() {
+  var target = document.querySelector('#nested .target')
+  return target;
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list');
+for (let i = 0, l = rankedLists.length; i < l; i++) {
+  rankedLists[i].innerHTML = (i + n).toString();
+}
+}
+
+function deepestChild() {
+var  element = document.querySelector('#grand-node div div div div');
+  return element;
 }
